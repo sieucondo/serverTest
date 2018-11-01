094d976b22696862dbdcf676727798a7cdecae44
sieucondo <44018235+sieucondo@users.noreply.github.com>
2018-11-01 14:54:21 +0700

    Initial commit

---
+++ api/db.js
@@ -0,0 +1,12 @@
+'use strict';
+const mysql = require('mysql');
+
+const db = mysql.createConnection({
+  host: process.env.DB_HOST || "localhost",
+  user: process.env.DB_USER || "root",
+  password: process.env.DB_PASSWORD || "123456",
+  database: process.env.DB_NAME || "fastorder",
+  multipleStatements: true
+});
+
+module.exports = db
