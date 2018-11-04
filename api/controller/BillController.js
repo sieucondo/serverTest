9aa6fb704672f61856f243acf238d7216afcf167
sieucondo <44018235+sieucondo@users.noreply.github.com>
2018-11-04 18:13:32 +0700

    sieu merge

---
+++ api/controller/BillController.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const util = require('util')
+const mysql = require('mysql')
+const db = require('./../db')
+
+module.exports = {
+    // hàm tạo và trả về billId vừa được tạo
+    createBill: (req, res) => {
+        // let data = req.body;
+        let sql = 'SET @TableKey=?; \
+        CALL `CreateNewBill`(@TableKey);';
+        db.query(sql, [req.params.tableKey], (err, response) => {
+            if (err) throw err
+            // rows.forEach(element => {
+            //     if (element.constructor == Array) {
+            res.json(response[1]);
+        })
+    },
+    addProductsToBillDetail: (req, res) => {
+        let sql = 'SET @BillId = ?;SET @ProductId = ?;SET @Quantity = ?;\
+            CALL `AddToBillDetail`(@BillId, @ProductId, @Quantity);';
+        db.query(sql, [req.params.BillId, req.params.ProductId, req.params.Quantity], (err, response) => {
+            if (err) throw err
+            res.json({ message: 'Inserted successfully!' })
+        })
+    }
+}
