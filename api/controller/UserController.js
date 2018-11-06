0afe12d7ff81b6110cde11841d3734222f71745f
sieucondo <44018235+sieucondo@users.noreply.github.com>
2018-11-06 15:14:28 +0700

    modify store procedure, add getBillByStoreId

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -25,7 +25,12 @@ module.exports = {
     },
 
     addUser :(req, res) => {
-        let sql = 'SET @UserName = ? ;SET @Fullname = ?;SET @Address = ?;SET @StoreId = ?;SET @RoleId = ?;SET @Password = ?;\
+        let sql = ' SET @UserName = ? ;\
+                    SET @Fullname = ?;\
+                    SET @Address = ?;\
+                    SET @StoreId = ?;\
+                    SET @RoleId = ?;\
+                    SET @Password = ?;\
         CALL `addUser`(@UserName ,@Fullname ,@Address ,@StoreId ,@RoleId,@Password)';
         db.query(sql,[req.params.Username,req.params.FullName,
             req.params.Address,req.params.StoreId,req.params.RoleId,req.params.Password], (err, response) => {
